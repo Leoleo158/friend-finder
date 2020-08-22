e9744f1b3978e3125bb7491cbd6841fec734e2cd
Leo Gervilla <leo.gervilla@getweave.com@Leos-MacBook-Pro-242.local>
2020-08-22 15:48:04 -0600

    listener and starter for server set

--- server.js
+++ server.js
@@ -15,3 +15,8 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 //ROUTER
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
+
+// Starts the server
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
+  });
